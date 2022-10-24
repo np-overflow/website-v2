78a99b9f0007f0871a4ac86a2089a311e85b147c
jimmy <s10219505@connect.np.edu.sg>
2022-10-24 18:42:15 +0800

    feat: add menu item icons + update styles

--- layouts/default.vue
+++ layouts/default.vue
@@ -8,13 +8,14 @@ const {data: meta} = await useAsyncData('meta', () => queryContent('/meta').find
     <div class="navbar bg-primary">
         <div class="navbar-start">
             <div class="dropdown">
-                <label tabindex="0" class="btn btn-ghost lg:hidden">
+                <label tabindex="0" class="btn btn-ghost rounded lg:hidden">
                     <Icon size="26" name="heroicons:bars-3-bottom-left"/>
                 </label>
                 <ul tabindex="0"
-                    class="menu menu-compact dropdown-content mt-3 p-2 bg-primary shadow rounded-box w-52">
-                    <li v-for="{ name, to } in meta.menu_items">
-                        <NuxtLink :to="to">
+                    class="menu menu-compact dropdown-content mt-3 p-2 bg-primary shadow rounded-md w-52">
+                    <li v-for="{ name, to, icon } in meta.menu_items">
+                        <NuxtLink :to="to" class="!rounded">
+                            <Icon size="16" :name="icon" />
                             {{ name }}
                         </NuxtLink>
                     </li>
@@ -22,13 +23,14 @@ const {data: meta} = await useAsyncData('meta', () => queryContent('/meta').find
             </div>
 
             <img alt="Overflow logo" width="50" height="50" :src="meta.logo_url"/>
-            <a class="btn btn-ghost normal-case text-xl">{{ meta.title }}</a>
+            <a class="btn btn-ghost rounded normal-case text-xl">{{ meta.title }}</a>
         </div>
 
         <div class="navbar-center hidden lg:flex">
             <ul class="menu menu-horizontal p-0">
-                <li v-for="{ name, to } in meta.menu_items">
-                    <NuxtLink :to="to">
+                <li v-for="{ name, to, icon } in meta.menu_items">
+                    <NuxtLink :to="to" class="!rounded">
+                        <Icon size="16" :name="icon" />
                         {{ name }}
                     </NuxtLink>
                 </li>
@@ -36,7 +38,7 @@ const {data: meta} = await useAsyncData('meta', () => queryContent('/meta').find
         </div>
 
         <div class="navbar-end">
-            <NuxtLink :to="meta.join_url" class="btn btn-ghost">
+            <NuxtLink :to="meta.join_url" class="btn btn-ghost rounded">
                 Join Us!
             </NuxtLink>
         </div>
