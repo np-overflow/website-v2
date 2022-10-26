9507f66ddc73c3a616abb5bf279f26db37ef2564
Qin Guan <qinguan20040914@gmail.com>
2022-10-26 10:51:42 +0800

    wip team

---
+++ pages/team.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+const { data: team } = await useAsyncData('team', () => queryContent('/team').findOne())
+</script>
+
+<template>
+	<div class="container mx-auto py-10">
+		<h1 class="text-5xl font-cubano">
+			The
+			<span
+				class="text-transparent bg-clip-text bg-gradient-to-tr from-amber-500 to-pink-500"
+			>
+				Team
+			</span>
+		</h1>
+
+		<div class="grid grid-cols-1 md:grid-cols-3">
+			<div v-for="{ name, role } in team.exco" :key="name" class="mt-5">
+				<b>{{ name }}</b>
+				<br>
+				<span>{{ role }}</span>
+			</div>
+		</div>
+	</div>
+</template>
