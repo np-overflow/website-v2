5808f79e736c33e06ad34efacc2bad34bf87b562
jimmy <s10219505@connect.np.edu.sg>
2022-10-27 15:17:17 +0800

    feat: update navbar border

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,18 +7,37 @@ const { data: meta } = await useAsyncData('meta', () => queryContent('/meta').fi
 
 <template>
 	<div class="font-poppins">
-		<div class="sticky top-0 z-10 font-cubano h-12">
+		<div class="sticky top-0 z-10 font-cubano">
 			<div class="nav-glass" />
-			<div class="navbar relative m-auto max-w-7xl shadow-glass dark:shadow-glass-dark">
-				<div class="navbar-start">
-					<div class="dropdown">
-						<label tabindex="0" class="btn btn-ghost rounded lg:hidden">
-							<Icon size="26" name="heroicons:bars-3-bottom-left" />
-						</label>
-						<ul
-							tabindex="0"
-							class="menu menu-compact bg-base-100 dropdown-content mt-3 p-2 shadow-md rounded-md w-52"
-						>
+			<div class="relative m-auto max-w-7xl nav-border w-full flex flex-col">
+				<div class="navbar h-12">
+					<div class="navbar-start">
+						<div class="dropdown">
+							<label tabindex="0" class="btn btn-ghost rounded lg:hidden">
+								<Icon size="26" name="heroicons:bars-3-bottom-left" />
+							</label>
+							<ul
+								tabindex="0"
+								class="menu menu-compact bg-base-100 dropdown-content mt-3 p-2 shadow-md rounded-md w-52"
+							>
+								<li v-for="{ name, to, icon } in meta.menu_items" :key="to">
+									<NuxtLink :to="to" class="!rounded">
+										<Icon size="16" :name="icon" />
+										{{ name }}
+									</NuxtLink>
+								</li>
+							</ul>
+						</div>
+
+						<TheOverflowLogo :urls="meta.logos" />
+
+						<NuxtLink to="/" class="btn btn-ghost rounded normal-case text-xl invisible sm:visible font-medium">
+							{{ meta.title }}
+						</NuxtLink>
+					</div>
+
+					<div class="navbar-center hidden lg:flex">
+						<ul class="menu menu-horizontal p-0">
 							<li v-for="{ name, to, icon } in meta.menu_items" :key="to">
 								<NuxtLink :to="to" class="!rounded">
 									<Icon size="16" :name="icon" />
@@ -28,31 +47,14 @@ const { data: meta } = await useAsyncData('meta', () => queryContent('/meta').fi
 						</ul>
 					</div>
 
-					<TheOverflowLogo :urls="meta.logos" />
+					<div class="navbar-end">
+						<TheThemeToggle />
 
-					<NuxtLink to="/" class="btn btn-ghost rounded normal-case text-xl invisible sm:visible font-medium">
-						{{ meta.title }}
-					</NuxtLink>
-				</div>
-
-				<div class="navbar-center hidden lg:flex">
-					<ul class="menu menu-horizontal p-0">
-						<li v-for="{ name, to, icon } in meta.menu_items" :key="to">
-							<NuxtLink :to="to" class="!rounded">
-								<Icon size="16" :name="icon" />
-								{{ name }}
-							</NuxtLink>
-						</li>
-					</ul>
-				</div>
-
-				<div class="navbar-end">
-					<TheThemeToggle />
-
-					<NuxtLink :to="meta.join_url" class="btn btn-ghost rounded">
-						<Icon size="16" name="heroicons:user-plus-solid" class="mr-2" />
-						Join Us!
-					</NuxtLink>
+						<NuxtLink :to="meta.join_url" class="btn btn-ghost rounded">
+							<Icon size="16" name="heroicons:user-plus-solid" class="mr-2" />
+							Join Us!
+						</NuxtLink>
+					</div>
 				</div>
 			</div>
 		</div>
@@ -65,10 +67,14 @@ const { data: meta } = await useAsyncData('meta', () => queryContent('/meta').fi
 
 <style scoped>
 	.nav-glass::before, .nav-glass::after {
-		@apply content-default absolute -top-0.25 inset-x-0 -bottom-1/2;
+		@apply content-default absolute top-[-1px] inset-x-0 -bottom-1/2;
 	}
 	.nav-glass::before {
 		@apply backdrop-blur-glass;
 		mask-image: linear-gradient(to bottom,black 48px,transparent)
+	}
+
+	.nav-border::after {
+		@apply content-default inset-x-0 bottom-0 h-px bg-[#5b5b5b14] dark:bg-[#ffffff25] w-full;
 	}
 </style>
