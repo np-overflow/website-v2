b79051070c5c5bfa82fecd9373b8ce254299aacc
Qin Guan <qinguan20040914@gmail.com>
2022-10-30 01:31:05 +0800

    debug

--- server/routes/og/workshops.ts
+++ server/routes/og/workshops.ts
@@ -4,23 +4,28 @@ import { $fetch } from 'ohmyfetch'
 import initYoga from 'yoga-wasm-web'
 
 export default defineEventHandler(async () => {
-	const interV = await $fetch('https://unpkg.com/@fontsource/inter@4.5.14/files/inter-all-500-normal.woff').then(r => r.arrayBuffer())
-	const wasm = await $fetch('https://unpkg.com/yoga-wasm-web@0.1.2/dist/yoga.wasm').then(r => r.arrayBuffer())
+	try {
+		const interV = await $fetch('https://unpkg.com/@fontsource/inter@4.5.14/files/inter-all-500-normal.woff').then(r => r.arrayBuffer())
+		const wasm = await $fetch('https://unpkg.com/yoga-wasm-web@0.1.2/dist/yoga.wasm').then(r => r.arrayBuffer())
 
-	const yoga = await initYoga(wasm)
-	await init(yoga)
+		const yoga = await initYoga(wasm)
+		await init(yoga)
 
-	return await satori({
-		type: 'div',
-		props: {
-			children: 'hello, world',
-			style: { color: 'black' },
-		},
-	}, {
-		height: 300,
-		fonts: [{
-			data: interV as ArrayBuffer,
-			name: 'Inter',
-		}],
-	})
+		return await satori({
+			type: 'div',
+			props: {
+				children: 'hello, world',
+				style: { color: 'black' },
+			},
+		}, {
+			height: 300,
+			fonts: [{
+				data: interV as ArrayBuffer,
+				name: 'Inter',
+			}],
+		})
+	}
+	catch (e) {
+		return e.message || JSON.stringify(e)
+	}
 })
