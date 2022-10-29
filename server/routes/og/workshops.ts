a1ed3d5d9a9b34a3488841662824a9464eefe845
Qin Guan <qinguan20040914@gmail.com>
2022-10-30 01:25:09 +0800

    fix: test wasm

--- server/routes/og/workshops.ts
+++ server/routes/og/workshops.ts
@@ -1,8 +1,14 @@
-import satori from 'satori'
+import satori, { init } from 'satori/wasm'
 import { $fetch } from 'ohmyfetch'
+
+import initYoga from 'yoga-wasm-web'
 
 export default defineEventHandler(async () => {
 	const interV = await $fetch('https://unpkg.com/@fontsource/inter@4.5.14/files/inter-all-500-normal.woff').then(r => r.arrayBuffer())
+	const wasm = await $fetch('https://unpkg.com/yoga-wasm-web@0.1.2/dist/yoga.wasm').then(r => r.arrayBuffer())
+
+	const yoga = await initYoga(wasm)
+	await init(yoga)
 
 	return await satori({
 		type: 'div',
