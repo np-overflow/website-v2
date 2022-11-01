c4025eecac3c1cc817b53f967cb7ee4322ceeca1
Qin Guan <qinguan20040914@gmail.com>
2022-11-01 14:58:15 +0800

    fix: bad weights

--- components/TimelineCard.vue
+++ components/TimelineCard.vue
@@ -42,7 +42,7 @@ const parsedDate = computed(() => {
 						<div class="w-1/2">
 							<NuxtLink
 								:to="props.classUrl"
-								class="btn-accent btn-sm btn w-full rounded font-cubano text-xs text-white"
+								class="btn-accent btn-sm btn w-full rounded font-cubano text-xs font-medium text-white"
 							>
 								Learn more
 							</NuxtLink>
@@ -107,28 +107,29 @@ const parsedDate = computed(() => {
 </template>
 
 <style scoped>
-    .right-card::after {
-        @apply content-default rounded-full w-4 h-4 bg-base-100 border-2 border-base-content absolute top-5 sm:top-card-offset;
-        left: -9px;
-    }
+.right-card::after {
+	@apply content-default rounded-full w-4 h-4 bg-base-100 border-2 border-base-content absolute top-5 sm:top-card-offset;
+	left: -9px;
+}
 
-	.card-body::before {
-		@apply content-default p-px absolute inset-0;
-		border-radius: inherit;
-		background: linear-gradient(#ffffff15, rgba(255, 255, 255, .0) 120%);
-		mask: linear-gradient(black, black) content-box content-box, linear-gradient(black, black);
-		-webkit-mask-composite: xor;
-	}
+.card-body::before {
+	@apply content-default p-px absolute inset-0;
+	border-radius: inherit;
+	background: linear-gradient(#ffffff15, rgba(255, 255, 255, .0) 120%);
+	mask: linear-gradient(black, black) content-box content-box, linear-gradient(black, black);
+	-webkit-mask-composite: xor;
+}
 
-	.card-body {
-		background: radial-gradient(ellipse at center,rgba(0, 225, 244, 0.15), rgba(255, 255, 255 ,0))
-	}
+.card-body {
+	background: radial-gradient(ellipse at center, rgba(0, 225, 244, 0.15), rgba(255, 255, 255, 0))
+}
 
-	html[data-theme="light"] .card-body::before {
-		background: linear-gradient(#00000015, rgba(160, 160, 160, .0) 120%);
-		mask: linear-gradient(white, white) content-box content-box, linear-gradient(white, white);
-	}
-	html[data-theme="light"] .card-body {
-		background: radial-gradient(ellipse at center,rgba(211, 105, 53, 0.3), rgba(0, 0, 0 ,0))
-	}
+html[data-theme="light"] .card-body::before {
+	background: linear-gradient(#00000015, rgba(160, 160, 160, .0) 120%);
+	mask: linear-gradient(white, white) content-box content-box, linear-gradient(white, white);
+}
+
+html[data-theme="light"] .card-body {
+	background: radial-gradient(ellipse at center, rgba(211, 105, 53, 0.3), rgba(0, 0, 0, 0))
+}
 </style>
