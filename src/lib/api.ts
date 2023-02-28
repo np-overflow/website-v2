efd4f42eb6b9f03b81beeddc01b72cba712382d1
Qin Guan <qinguan20040914@gmail.com>
2023-03-01 00:36:11 +0800

    feat: seo improvements

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,8 +1,10 @@
 import { load } from 'js-yaml'
 
 import rawMeta from '../content/meta.yaml?raw';
+import rawSEO from '../content/seo.yaml?raw'
 
 export interface Meta {
+  // Title used for the header on the website
   title: string
   logos: {
     light: string
@@ -22,3 +24,9 @@ export function getMeta() {
   return load(rawMeta) as Meta
 }
 
+export interface SEO extends SEOMeta {
+}
+
+export function getSEO() {
+  return load(rawSEO) as SEO
+}
