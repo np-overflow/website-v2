cb22f5bf09b52aac9f49ea76e5e7825fcddf8a9b
Arash <47273556+arashnrim@users.noreply.github.com>
2023-03-23 22:46:36 +0800

    feat: add new page for SIG Fiesta 2023

---
+++ pages/sig-fiesta-2023.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+useHead({
+  title: "SIG Fiesta 2023",
+});
+</script>
+
+<template>
+  <section id="hero" class="container mx-auto py-10 px-4">
+    <section class="min-h-[33vh] flex flex-col justify-center">
+      <h1
+        class="font-cubano text-5xl bg-gradient-to-tr from-amber-500 to-pink-500 bg-clip-text text-transparent"
+      >
+        SIG Fiesta 2023
+      </h1>
+      <p class="mt-4 w-1/2">
+        Come and get to know more about what we do at Overflow from the very
+        people part of it! Have a look at what you can expect when you join
+        Overflow and snag some freebies while you're there.
+      </p>
+    </section>
+  </section>
+</template>
