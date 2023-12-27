ac96fd5c1ba3ef28ea1f84230e326d7fd66555e3
Arash <47273556+arashnrim@users.noreply.github.com>
2023-12-28 00:03:49 +0800

    chore(deps)!: upgrade dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,12 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
-import image from "@astrojs/image";
-
-import robotsTxt from 'astro-robots-txt'
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://np-overflow.club',
-  integrations: [tailwind(), image(), robotsTxt()]
+  site: "https://np-overflow.club",
+  integrations: [tailwind(), robotsTxt()],
 });
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,8 +1,8 @@
 /// <reference path="../.astro/types.d.ts" />
-/// <reference types="@astrojs/image/client" />
+/// <reference types="astro/client" />
 
 interface SEOMeta {
-  title: string
-  description: string
-  image: string
+  title: string;
+  description: string;
+  image: string;
 }
