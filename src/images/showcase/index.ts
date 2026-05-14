7295f450c65eb161738a885cbeef1f40e9fcb573
Hong Yi <limhongyi0@gmail.com>
2026-05-14 22:31:39 +0800

    update: Add project images and description

--- src/images/showcase/index.ts
+++ src/images/showcase/index.ts
@@ -1,8 +1,10 @@
 // Eye Can Drive
-export {default as EyeCanDrive_1 } from "./EyeCanDrive/drivesim.jpg"
+export {default as EyeCanDrive1 } from "./EyeCanDrive/EyeCanDrive1.png"
+export {default as EyeCanDrive2 } from "./EyeCanDrive/EyeCanDrive2.jpg"
 
 // Digital Photobooth
-export {default as DigitalPhotobooth_1} from "./DigitalPhotobooth/DigitalPhotobooth_1.jpeg"
+export {default as DigitalPhotobooth1} from "./DigitalPhotobooth/DigitalPhotobooth1.jpeg"
 
 // Blaster Brawl
-export {default as BlasterBrawl_1} from "./BlasterBrawl/Blaster1.jpeg"
+export {default as BlasterBrawl1} from "./BlasterBrawl/Blaster1.jpeg"
+export {default as BlasterBrawl2} from "./BlasterBrawl/Blaster2.jpeg"
